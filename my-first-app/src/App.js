8c374e0fa82a447e51bf49aa2437795e3166ceb0
Martin Gundtoft <marg@kea.dk>
2021-09-15 21:58:27 +0200

    actions test

---
+++ my-first-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello CSCI E-39!</p>
+        <p>
+          My name is Martin Gundtoft (this time with actions i hope)
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
